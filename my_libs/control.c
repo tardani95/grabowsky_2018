ca58f7c70fc7b546b89e67a826a06ae082b57ff2
Daniel Tar <tardani95@gmail.com>
2018-10-01 17:23:23 +0200

    defines added - p term modified

--- my_libs/control.c
+++ my_libs/control.c
@@ -2,20 +2,23 @@
 
 
 /* control loop @1kHz */
-
+#define RS 0
+#define LS 3
+#define RF 2
+#define LF 1
 
 //float T=0.001; // @1   kHz
 float T=0.002; // @0.5 kHz
 
-float Kp=1.5; /* P */
-float Ki=0.05; /* I */
-float Kd=0.1; /* D */
+float Kp=0.25; /* P */ //1.5
+float Ki=0.0; /* I */ // 0.05
+float Kd=0.0; /* D */ //0.1
 
 float e=0;
 float e_d=0;
 
-//float e_max=2000.0;
-float e_max = 4000.0;
+float e_max=2500.0;
+//float e_max = 4000.0;
 float I=0;
 float D=0;
 
@@ -67,18 +70,18 @@ void TIM4_IRQHandler(void){
 
         e_d=e;
 
-        e=(ENC_GetLeftValue()-ENC_GetRightValue())/e_max; // L-R
+//        e=(ENC_GetLeftValue()-ENC_GetRightValue())/e_max; // L-R
+        e = (adcBuf[LS] - adcBuf[RS]) / e_max;
 
         D=Kd*(e-e_d)/T;
 
         I+=Ki*e;
-
         if(I>Imax)
         	I=Imax;
         if(I<-Imax)
         	I=-Imax;
-
         I=0;
+
         s=Kp*e+I+D;
 
 
